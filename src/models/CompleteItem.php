f581a01d5f1db6154966c086794b68cea8dbe900
Andrew Welch <andrew@nystudio107.com>
2022-07-24 14:05:05 -0400

    refactor: Remove `FluentModel` class and replace the magic method setter with fluent setter methods in `CompleteItem`

--- src/models/CompleteItem.php
+++ src/models/CompleteItem.php
@@ -10,9 +10,9 @@
 
 namespace nystudio107\twigfield\models;
 
+use craft\base\Model;
 use craft\validators\ArrayValidator;
 use nystudio107\twigfield\base\AutocompleteInterface;
-use nystudio107\twigfield\base\FluentModel;
 use nystudio107\twigfield\types\CompleteItemKind;
 
 /**
@@ -20,26 +20,23 @@ use nystudio107\twigfield\types\CompleteItemKind;
  *
  * @author    nystudio107
  * @package   Twigfield
- * @since     1.0.0
- *
- * @method CompleteItem additionalTextEdits(string $additionalTextEdits) An optional array of additional text edits that are applied when selecting this completion. Edits must not overlap with the main edit nor with themselves.
- * @method CompleteItem command(array $command) A command that should be run upon acceptance of this item.
- * @method CompleteItem commitCharacters(array $commitCharacters) An optional set of characters that when pressed while this completion is active will accept it first and then type that character. Note that all commit characters should have `length=1` and that superfluous characters will be ignored.
- * @method CompleteItem detail(string $detail) A human-readable string with additional information about this item, like type or symbol information.
- * @method CompleteItem documentation(string $documentation) A human-readable string that represents a doc-comment.
- * Can contain Markdown
- * @method CompleteItem filterText(string $filterText) A string that should be used when filtering a set of completion items. When falsy the `label` is used.
- * @method CompleteItem insertText(string $insertText) A string or snippet that should be inserted in a document when selecting this completion.
- * @method CompleteItem insertTextRules(int $insertTextRules) Additional rules (as bitmask) that should be applied when inserting this completion.
- * @method CompleteItem kind(int $kind) The kind of this completion item. Based on the kind an icon is chosen by the editor.
- * @method CompleteItem label(string $label) The label of this completion item. By default this is also the text that is inserted when selecting this completion.
- * @method CompleteItem preselect(bool $preselect) Select this item when showing. Note that only one completion item can be selected and that the editor decides which item that is. The rule is that the first item that matches best is selected.
- * @method CompleteItem range(array $range) A range of text that should be replaced by this completion item.
- * @method CompleteItem sortText(string $sortText) A string that should be used when comparing this item with other items. When falsy the `label` is used.
- * @method CompleteItem tags(array $tags) A modifier to the kind which affect how the item is rendered, e.g. Deprecated is rendered with a strikeout
+ * @since     1.0.11
  */
-class CompleteItem extends FluentModel
+class CompleteItem extends Model
 {
+    // Public Static Methods
+    // =========================================================================
+
+    /**
+     * Factory method for complete item objects
+     *
+     * @return CompleteItem
+     */
+    public static function create(): CompleteItem
+    {
+        return new CompleteItem();
+    }
+
     // Public Properties
     // =========================================================================
 
@@ -124,21 +121,159 @@ class CompleteItem extends FluentModel
      */
     public $tags;
 
-    // Public Static Methods
+    // Public Methods
     // =========================================================================
 
     /**
-     * Factory method for complete item objects
-     *
-     * @return CompleteItem
+     * An optional array of additional text edits that are applied when selecting this completion.
+     * Edits must not overlap with the main edit nor with themselves.
+     * ref: https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ISingleEditOperation.html
+     * @param $value array
      */
-    public static function create(): CompleteItem
+    public function additionalTextEdits($value): self
     {
-        return new CompleteItem();
+        $this->additionalTextEdits = $value;
+        return $this;
     }
 
-    // Public Methods
-    // =========================================================================
+    /**
+     * A command that should be run upon acceptance of this item.
+     * ref: https://microsoft.github.io/monaco-editor/api/interfaces/monaco.languages.Command.html
+     * @param $value array
+     */
+    public function command($value): self
+    {
+        $this->command = $value;
+        return $this;
+    }
+
+    /**
+     * An optional set of characters that when pressed while this completion is active will accept
+     * it first and then type that character. Note that all commit characters should have `length=1` and that
+     * superfluous characters will be ignored.
+     * @param $value array
+     */
+    public function commitCharacters($value): self
+    {
+        $this->commitCharacters = $value;
+        return $this;
+    }
+
+    /**
+     * A human-readable string with additional information about this item, like type or symbol information.
+     * @param $value string
+     */
+    public function detail($value): self
+    {
+        $this->detail = $value;
+        return $this;
+    }
+
+    /**
+     * A human-readable string that represents a doc-comment.
+     * Can contain Markdown
+     * @param $value string
+     */
+    public function documentation($value): self
+    {
+        $this->documentation = $value;
+        return $this;
+    }
+
+    /**
+     * A string that should be used when filtering a set of completion items.
+     * When falsy the `label` is used.
+     * @param $value string
+     */
+    public function filterText($value): self
+    {
+        $this->filterText = $value;
+        return $this;
+    }
+
+    /**
+     * A string or snippet that should be inserted in a document when selecting this completion.
+     * @param $value string
+     */
+    public function insertText($value): self
+    {
+        $this->insertText = $value;
+        return $this;
+    }
+
+    /**
+     * Additional rules (as bitmask) that should be applied when inserting this completion.
+     * @param $value int
+     */
+    public function insertTextRules($value): self
+    {
+        $this->insertTextRules = $value;
+        return $this;
+    }
+
+    /**
+     * The kind of this completion item. Based on the kind an icon is chosen by the editor.
+     * @param $value int
+     */
+    public function kind($value): self
+    {
+        $this->kind = $value;
+        return $this;
+    }
+
+    /**
+     * The label of this completion item. By default this is also the text that is inserted
+     * when selecting this completion.
+     * @param $value string
+     */
+    public function label($value): self
+    {
+        $this->label = $value;
+        return $this;
+    }
+
+    /**
+     * Select this item when showing. Note that only one completion item can be selected and that
+     * the editor decides which item that is. The rule is that the first item of those that match best is selected.
+     * @param $value bool
+     */
+    public function preselect($value): self
+    {
+        $this->preselect = $value;
+        return $this;
+    }
+
+    /**
+     * A range of text that should be replaced by this completion item.
+     * @param $value array
+     */
+    public function range($value): self
+    {
+        $this->range = $value;
+        return $this;
+    }
+
+    /**
+     * A string that should be used when comparing this item with other items. When falsy
+     * the `label` is used.
+     * @param $value string
+     */
+    public function sortText($value): self
+    {
+        $this->sortText = $value;
+        return $this;
+    }
+
+    /**
+     * A modifier to the kind which affect how the item is rendered, e.g. Deprecated is rendered
+     * with a strikeout
+     * @param $value array
+     */
+    public function tags($value): self
+    {
+        $this->tags = $value;
+        return $this;
+    }
 
     /**
      * Add the completion item to the passed in AutocompleteInterface static class
