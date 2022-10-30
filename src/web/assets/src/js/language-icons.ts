2041972d603dd5f6f0692e6d9177d4f21ba9f840
Andrew Welch <andrew@nystudio107.com>
2022-10-30 19:16:58 -0400

    refactor: Add TypeScript icon

--- src/web/assets/src/js/language-icons.ts
+++ src/web/assets/src/js/language-icons.ts
@@ -6,6 +6,7 @@ export const languageIconTitles: StringMap = {
   'twig': 'Twig code is supported',
   'javascript': 'JavaScript code is supported',
   'markdown': 'Markdown code is supported',
+  'typescript': 'TypeScript code is supported',
 }
 
 export const languageIcons: StringMap = {
@@ -34,5 +35,11 @@ export const languageIcons: StringMap = {
   <svg xmlns="http://www.w3.org/2000/svg" width="208" height="128" viewBox="0 0 208 128">
   <style type="text/css">.st0{fill:currentcolor;}</style>
   <rect class="st0" width="198" height="118" x="5" y="5" ry="10" stroke="#000" stroke-width="10"/><path d="M30 98V30h20l20 25 20-25h20v68H90V59L70 84 50 59v39zm125 0l-30-33h20V30h20v35h20z"/>
+  </svg>`,
+  // TypeScript
+  'typescript': `
+    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px">
+    <style type="text/css">.st0{fill:currentcolor;}</style>
+      <path class="st0" d="M45,4H5C4.447,4,4,4.448,4,5v40c0,0.552,0.447,1,1,1h40c0.553,0,1-0.448,1-1V5C46,4.448,45.553,4,45,4z M29,26.445h-5V42h-4\tV26.445h-5V23h14V26.445z M30.121,41.112v-4.158c0,0,2.271,1.712,4.996,1.712c2.725,0,2.62-1.782,2.62-2.026\tc0-2.586-7.721-2.586-7.721-8.315c0-7.791,11.25-4.717,11.25-4.717l-0.14,3.704c0,0-1.887-1.258-4.018-1.258s-2.9,1.013-2.9,2.096\tc0,2.795,7.791,2.516,7.791,8.141C42,44.955,30.121,41.112,30.121,41.112z"/>
   </svg>`,
 };
